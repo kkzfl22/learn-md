4000ffa2e7f90a4c5e1370264c9455d15a4bf520
liujun <546064298@qq.com>
2023-06-18 16:35:32 +0800

    源码学习结束去掉无用的包。

--- spring-securty/nullnull-security-management/src/main/java/com/nullnull/mapper/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.nullnull.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.nullnull.domain.User;
-
-public interface UserMapper extends BaseMapper<User> {
-}
