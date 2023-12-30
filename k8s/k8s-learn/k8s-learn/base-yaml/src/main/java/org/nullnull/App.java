86e2497cb34883e9959fb61d61fde963f71c09ef
liujun <546064298@qq.com>
2023-12-31 00:36:10 +0800

    使用k8s的yml文件运行相关的资源。到pod

---
+++ k8s/k8s-learn/k8s-learn/base-yaml/src/main/java/org/nullnull/App.java
@@ -0,0 +1,13 @@
+package org.nullnull;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
