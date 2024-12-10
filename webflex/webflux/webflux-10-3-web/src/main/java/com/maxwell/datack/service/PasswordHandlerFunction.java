aecfd96a78e5fd0f2f48ebd50acbb3bf5d88bbba
liujun <546064298@qq.com>
2024-12-10 23:22:44 +0800

    学习至204页，基于WEBClient的非阻塞跨服务通信。

---
+++ webflex/webflux/webflux-10-3-web/src/main/java/com/maxwell/datack/service/PasswordHandlerFunction.java
@@ -0,0 +1,31 @@
+package com.maxwell.datack.service;
+
+import com.maxwell.datack.dto.PasswordDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.HandlerFunction;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author nullnull
+ * @since 2024/12/10
+ */
+@Component
+public class PasswordHandlerFunction implements HandlerFunction {
+
+    private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder(18);
+
+    @Override
+    public Mono<Void> handle(ServerRequest serverRequest) {
+        return serverRequest
+                .bodyToMono(PasswordDTO.class)
+                .map(p -> passwordEncoder.matches(p.getRaw(), p.getSecured()))
+                .flatMap(isMatched -> isMatched
+                        ? ServerResponse.ok().build()
+                        : ServerResponse.status(HttpStatus.EXPECTATION_FAILED).build()
+                ).then(Mono.empty());
+    }
+}
